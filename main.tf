4c8ca137201aee2a8ae53f724ba1094470f3711d
ckonkol <109977093+ckonkol3@users.noreply.github.com>
2022-11-23 11:51:30 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ module "vpc" {
   enable_nat_gateway = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
@@ -45,12 +45,12 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.2"
-  name = "blog"
+  name    = "blog"
   
-  vpc_id = module.vpc.public_subnets[0]
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  vpc_id              = module.blog_vpc.vpc_id
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   
-  egress_rules = ["all-all"]
+  egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
